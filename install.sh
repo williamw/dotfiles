7e07c79cb88c8981e2bdcf3e1eaca2ef2fb2b385
Bill Welense <billw@modular.com>
2025-10-28 10:44:40 -0500

    Add install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,24 @@
+#!/bin/bash
+
 # PLACEHOLDER
 # This file will be used to install tools
 
+# nvim + lazyvim
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    brew install neovim
+elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    sudo add-apt-repository -y ppa:neovim-ppa/stable
+    sudo apt update
+    sudo apt install -y neovim
+fi
+
+# uv
+
+# nvm
+
+# pnpm
+
 # NOTE: Double-check how the pnpm install script works
-#   We don't want it to override the pnpm config we
-#   already have in .zshrc
+#       We don't want it to override the pnpm config we
+#       already have in .zshrc
 # pnpm config set global-bin-dir "$HOME/.local/share/pnpm"
