ef5749ed45e43c2ccc1f5ec0cd99892c630f92c5
Bill Welense <billw@modular.com>
2025-11-03 15:14:49 -0600

    Add 1password CLI

--- install.sh
+++ install.sh
@@ -29,6 +29,32 @@ else
     echo "✓ GitHub CLI is already installed."
 fi
 
+# 1Password CLI
+if ! command -v op &> /dev/null; then
+    echo "☁️Downloading 1Password CLI..."
+    if [[ "$OSTYPE" == "darwin"* ]]; then
+        brew install --cask 1password-cli
+    elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
+        curl -sS https://downloads.1password.com/linux/keys/1password.asc | \
+        sudo gpg --dearmor --output /usr/share/keyrings/1password-archive-keyring.gpg
+
+        echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/1password-archive-keyring.gpg] https://downloads.1password.com/linux/debian/$(dpkg --print-architecture) stable main" | \
+        sudo tee /etc/apt/sources.list.d/1password.list
+
+        sudo mkdir -p /etc/debsig/policies/AC2D62742012EA22/
+        curl -sS https://downloads.1password.com/linux/debian/debsig/1password.pol | \
+        sudo tee /etc/debsig/policies/AC2D62742012EA22/1password.pol
+
+        sudo mkdir -p /usr/share/debsig/keyrings/AC2D62742012EA22
+        curl -sS https://downloads.1password.com/linux/keys/1password.asc | \
+        sudo gpg --dearmor --output /usr/share/debsig/keyrings/AC2D62742012EA22/debsig.gpg
+
+        sudo apt-get update && sudo apt-get install -y 1password-cli
+    fi
+else
+    echo "✓ 1Password CLI is already installed."
+fi
+
 # nvim + lazyvim
 if ! command -v nvim &> /dev/null; then
     echo "☁️Downloading neovim..."
