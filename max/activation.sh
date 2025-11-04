8a706271f45cafa26f329012f5725ff437b2129b
Bill Welense <billw@modular.com>
2025-11-04 15:25:37 -0600

    Fix Pixi arg vs. env var passing

--- max/activation.sh
+++ max/activation.sh
@@ -8,32 +8,36 @@ if [ -z "$HF_TOKEN" ]; then
     echo "⚠️  Warning: Failed to retrieve HF_TOKEN from 1Password. Make sure you're authenticated with 'op signin'"
 fi
 
-MODEL_CLASS_1="OpenGVLab/InternVL3-14B-Instruct"
-MODEL_CLASS_2="Qwen/Qwen2.5-VL-7B-Instruct"
-MODEL_CLASS_3="HuggingFaceTB/SmolLM2-1.7B-Instruct"
-MODEL_Q_WEIGHTS="HuggingFaceTB/SmolLM2-1.7B-Instruct-GGUF/smollm2-1.7b-instruct-q4_k_m.gguf"
+CLASS_1_MODEL="OpenGVLab/InternVL3-14B-Instruct"
+CLASS_2_MODEL="Qwen/Qwen2.5-VL-7B-Instruct"
+CLASS_3_MODEL="HuggingFaceTB/SmolLM2-1.7B-Instruct"
+CLASS_3_WEIGHTS="HuggingFaceTB/SmolLM2-1.7B-Instruct-GGUF/smollm2-1.7b-instruct-q4_k_m.gguf"
 
 if command -v nvidia-smi &> /dev/null; then
     echo "Detected NVIDIA toolchain"
     compute_cap=$(nvidia-smi --query-gpu compute_cap --format=csv,noheader)
     echo "CUDA compute capacity: $compute_cap"
+    unset MAX_WEIGHTS
     if (( $(printf "%.0f" "$compute_cap") >= 9 )); then
-        MAX_MODEL=$MODEL_CLASS_1
+        export MAX_MODEL="--model $CLASS_1_MODEL"
     else
-        MAX_MODEL=$MODEL_CLASS_2
+        export MAX_MODEL="--model $CLASS_2_MODEL"
     fi
-    echo "Using MAX model: $MAX_MODEL"
 elif command -v rocm_agent_enumerator &> /dev/null; then
     echo "Detected AMD ROCm toolchain"
     gfx_arch=$(rocm_agent_enumerator | grep -v "^gfx000$" | head -n 1)
     echo "AMD GPU architecture: $gfx_arch"
+    unset MAX_WEIGHTS
     if [[ "$gfx_arch" =~ ^gfx94[2-9] ]]; then
-        MAX_MODEL=$MODEL_CLASS_1
+        export MAX_MODEL="--model $CLASS_1_MODEL"
     else
-        MAX_MODEL=$MODEL_CLASS_2
+        export MAX_MODEL="--model $CLASS_2_MODEL"
     fi
-    echo "Using MAX model: $MAX_MODEL"
 else
-    MAX_MODEL=$MODEL_CLASS_3
-    MAX_WEIGHTS=$MODEL_Q_WEIGHTS
+    echo "No NVIDIA or AMD toolchain detected"
+    echo "Default set to CPU-only model"
+    export MAX_MODEL="--model $CLASS_3_MODEL"
+    export MAX_WEIGHTS="--weight-path $CLASS_3_WEIGHTS"
 fi
+
+echo "max serve default args: $MAX_MODEL $MAX_WEIGHTS"
